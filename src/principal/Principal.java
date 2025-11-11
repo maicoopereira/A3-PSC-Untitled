ecfa9e8f1d801451bda2772bb17c8dbb2463e355
maicoopereira <maico.prr@gmail.com>
2025-11-10 21:16:04 -0300

    Commit inicial

---
+++ src/principal/Principal.java
@@ -0,0 +1,13 @@
+package principal;
+
+import view.TelaPrincipal;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        TelaPrincipal objetotela = new TelaPrincipal();
+        objetotela.setVisible(true);
+    }
+    
+    
+}
