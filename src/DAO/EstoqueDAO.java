300dc1721390101c115861279f155e9b4cfae04b
maicoopereira <maico.prr@gmail.com>
2025-11-11 21:27:30 -0300

    Updated the DAO Class, whith some CRUD methods

--- src/DAO/EstoqueDAO.java
+++ src/DAO/EstoqueDAO.java
@@ -2,10 +2,7 @@
 package DAO;
 import model.Produto;
 import java.util.ArrayList;
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.PreparedStatement;
-import java.sql.Timestamp;
+import java.sql.*;
 
 
 public class EstoqueDAO {
@@ -17,8 +14,8 @@ public class EstoqueDAO {
     //methods
     public Connection getEstoqueConnection() throws SQLException {     
         try {
-            Connection connection = DataBaseConnection.getConnection();
-            return connection;
+            Connection conn = DataBaseConnection.getConnection();
+            return conn;
 
         } catch (SQLException e) {
             System.out.println("Nao foi possivel conectar...");
@@ -26,8 +23,8 @@ public class EstoqueDAO {
         }
         
     }
-        //add product to the db.
-        public boolean InsertProdutoBD(Produto objeto) {
+    //add product to the db.
+    public void InsertProdutoBD(Produto objeto) {
         String sql = "INSERT INTO produtos(idprodutos,nomeproduto,descricaoproduto,quantidade,precoproduto, datadecadastro, datadeatualizacao) VALUES(?,?,?,?,?,?,?)";
 
 
@@ -45,19 +42,67 @@ public class EstoqueDAO {
             stmt.execute();
             stmt.close();
 
-            return true;
-
         } catch (SQLException erro) {
             throw new RuntimeException(erro);
         }
     }
         //remove a product from db
-        public boolean RemoveProdutoBD(Produto objeto) {
-            return false;
+    public void RemoveProdutoBD(Produto objeto) {
+        String sql = "DELETE FROM produtos WHERE produtoid="+objeto.getID;
+        int result;
+        try (Connection conn = DataBaseConnection.getConnection();
+             Statement stmt = conn.createStatement()){
+            stmt.executeUpdate(sql);
+        } catch (SQLException e) {
+            e.getMessage();
         }
         
-        //other methods...
+    }
+        
+        //return the bigger id in the db.
+    public int maiorID() {
+            
+        String sql = "SELECT MAX(ProdutoId) AS max_id FROM produtos";
 
+        try (Connection conn = DataBaseConnection.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql);
+             ResultSet result = stmt.executeQuery(sql)) {
+                if (result.next()) {
+                    return Integer.parseInt(result.getString("max_id"));
+                }
+
+        } catch (SQLException e) {
+            e.getMessage();
+        }
+        return 1;
+        }
     
+    //read method
+    
+    
+    
+    
+    //update method
+        public void atualizarDados(Produto objeto) {
+        
+        String sql = "UPDATE produtos SET nomeproduto = ?, descricaoproduto = ?, quantidade = ?, precoproduto = ?, datadeatualizacao = ?, categoria = ?, datadevalidade = ? WHERE ProdutoId = ?";
 
+        try (Connection conn = DataBaseConnection.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+            
+            stmt.setString(1, objeto.getNome);
+            stmt.setString(2, objeto.getDescricao);
+            stmt.setInt(3, objeto.getQuantidade);
+            stmt.setBigDecimal(4, objeto.getPreco);
+            stmt.setDate(5, objeto.getDataDeAtualizacao);
+            stmt.setString(6, objeto.getCategoria);
+            stmt.setDate(7, objeto.getDataDeValidade);
+            
+            stmt.execute();
+            stmt.close();
+
+        } catch (SQLException e) {
+            e.getMessage();
+        }
+    }
 }
